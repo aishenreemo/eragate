dd93c5e577120905d120c726b360b312fe8a40d6
Aivan <aish3n@pm.me>
2022-11-18 11:04:14 +0800

    chore: init `discord.js`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import discord from "discord.js";
+import dotenv from "dotenv";
+
+const intents = [discord.GatewayIntentBits.Guilds];
+const client = new discord.Client({ intents });
+
+function onReady() {
+    console.log("Bot is up and running!");
+}
+
+function main() {
+    dotenv.config();
+    client.on("ready", onReady);
+    client.login(process.env.ERAGATE_TOKEN);
+}
+
+main();
